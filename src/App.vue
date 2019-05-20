2633f66b6c66be5f9d7d48ee424d1ddd0d404870
邓天 <17820457171@163.com>
2019-05-20 09:35:17 +0800

    测试重构PC和移动端页面路由拦截分开判断模式

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,31 @@
   </div>
 </template>
 
+<script>
+export default {
+  name:'App',
+  data() {
+    return {
+      
+    }
+  },
+
+  methods:{
+    _isMobile(){
+  let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+  // localStorage.setItem('isiphone',flag)
+  localStorage.setItem('ismobile',flag?1:0)
+  return flag;
+},
+
+  }
+}
+</script>
+
+
+
+
+
 <style>
 html,body {
   width: 100%;
